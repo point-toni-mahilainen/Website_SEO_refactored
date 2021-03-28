f228dd8841af2d737a67948f9cbf4b84873f00c3
Toni Mahilainen <toni.mahilainen@profitsoftware.com>
2021-03-28 12:48:27 +0300

    Mainpage valmis

--- src/views/Mainpage.tsx
+++ src/views/Mainpage.tsx
@@ -1,18 +1,70 @@
-import React from "react";
 import { Grid } from "@material-ui/core";
+import ContactDetails from "components/ContactDetails";
+import OpeningHours from "components/OpeningHours";
 import { createUseStyles } from "react-jss";
-import Background from "img/back.jpg";
 
 const useStyles = createUseStyles({
-  container: {},
+  left: {
+    height: "calc(100vh - 120px)",
+    flexDirection: "column",
+  },
+  right: {
+    height: "calc(100vh - 120px)",
+  },
+  welcome: {
+    maxHeight: "30%",
+    color: "#ffffff",
+    fontSize: "2.2em",
+    textAlign: "center",
+    textShadow: "2px 2px #000000",
+  },
+  info: {
+    height: "fit-content",
+    color: "#ffffff",
+    padding: "15px",
+    textAlign: "center",
+    backgroundColor: "#2829A7",
+    borderRadius: "10px 0 0 10px",
+  },
 });
 
 const Mainpage = () => {
   const classes = useStyles();
 
   return (
-    <Grid className={classes.container}>
-      <h2>Mainpage</h2>
+    <Grid container>
+      <Grid container item xs={10} className={classes.left}>
+        <Grid
+          container
+          item
+          xs={12}
+          justifyContent="center"
+          alignContent="center"
+          className={classes.welcome}
+        >
+          <h1>
+            Tervetuloa SEO Kosken
+            <br />
+            kotisivuille
+          </h1>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        item
+        xs={2}
+        alignContent="center"
+        className={classes.right}
+      >
+        <Grid item className={classes.info}>
+          <Grid style={{ borderBottom: "1px solid white" }}>
+            <OpeningHours flexDirection="column" />
+          </Grid>
+          <Grid style={{ paddingTop: "15px" }}>
+            <ContactDetails />
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
